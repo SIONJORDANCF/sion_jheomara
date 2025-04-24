ad6b22f09e0c344ca4f81f331e49c9017bd2ebf8
JORDANSION <yordansioncossiofuentes@gmail.com>
2025-04-24 09:08:05 -0400

    peek

--- cola.py
+++ cola.py
@@ -9,7 +9,7 @@ class Cola :
         self.final = None
         self.tamanho = 0
 
-    def enqueue(self, valor):
+    def enqueue(self, valor): #este lo llame cola no por su nombre cuando hice el commit
         nuevo_nodo = Nodo(valor)
         if self.isEmpty():  # Si la cola está vacía
             self.frente = nuevo_nodo
@@ -22,10 +22,20 @@ class Cola :
     def desencolar(self):
         if self.is_empty():
             raise IndexError("No se puede desencolar de una cola vacía")
-        dato = self.frente.dato
+        dato = self.frente.valor
         self.frente = self.frente.siguiente
         if self.frente is None:
             self.final = None
-        self.contador -= 1
+        self.tamanho -= 1
         return dato
-       
+    
+    def is_empty(self):
+        return self.frente is None
+    
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("No se puede ver el frente de una cola vacía")
+        return self.frente.valor 
+    
+    #jheomara solo harias la size que el tamanho esa funcion nomaj te falta y ya estaria todo y si lo haces el metodo mostrar mas para poder
+    #mostra lo q se ejecuta vale solo implementas la funcion mostrar y ya estaria esos dos nomaj 
